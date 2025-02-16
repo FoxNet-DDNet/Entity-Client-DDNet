62374fc3e17a84055efeae10a57f462470f18f82
qxdFox <jusi.p@icloud.com>
2025-02-16 15:48:31 +0100

    fix ping circle not receiving players ping

--- src/game/client/components/controls.cpp
+++ src/game/client/components/controls.cpp
@@ -189,7 +189,7 @@ int CControls::SnapInput(int *pData)
 	else
 		m_aInputData[g_Config.m_ClDummy].m_PlayerFlags = PLAYERFLAG_PLAYING;
 
-	if(m_pClient->m_Scoreboard.IsActive())
+	if(m_pClient->m_Scoreboard.IsActive() || g_Config.m_ClPingNameCircle)
 		m_aInputData[g_Config.m_ClDummy].m_PlayerFlags |= PLAYERFLAG_SCOREBOARD;
 
 	if(Client()->ServerCapAnyPlayerFlag() && m_pClient->m_Controls.m_aShowHookColl[g_Config.m_ClDummy])
