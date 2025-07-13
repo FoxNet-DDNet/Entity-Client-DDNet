6b1599c71b92bd1819464ed54f8bcb2d37878c73
qxdFox <jusi.p@icloud.com>
2025-07-13 23:24:05 +0200

    fix merge

--- src/engine/textrender.h
+++ src/engine/textrender.h
@@ -152,6 +152,11 @@ namespace FontIcons {
 [[maybe_unused]] static const char *FONT_ICON_QUESTION = "?";
 
 [[maybe_unused]] static const char *FONT_ICON_CAMERA = "\xEF\x80\xB0";
+
+// E-Client
+[[maybe_unused]] static const char *FONT_ICON_COMMENT = "\xEF\x81\xB5";
+[[maybe_unused]] static const char *FONT_ICON_USERS = "\xEF\x83\x80";
+[[maybe_unused]] static const char *FONT_ICON_KEY = "\xEF\x82\x84";
 } // end namespace FontIcons
 
 enum ETextCursorSelectionMode
